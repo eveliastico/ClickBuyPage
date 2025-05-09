90ecb69e241c14851f2fd5e542ded069822a9916
Melito <premiumm9@gmail.com>
2025-05-09 16:38:25 -0700

    update inicios

--- src/components/inicio/inicioCliente/inicioCliente.js
+++ src/components/inicio/inicioCliente/inicioCliente.js
@@ -5,9 +5,11 @@ document.addEventListener("DOMContentLoaded", function () {
         //Despliega el menu
         menuDisplay.style.display = (menuDisplay.style.display === "none" || menuDisplay.style.display === "") ? "block" : "none";
     });
+
     document.addEventListener("click", function(event){
         if (!btnConfig.contains(event.target) && !menuDisplay.contains(event.target)) {
             menuDisplay.style.display = "none";
         }
     });
+
 });
