c70797861447b3e8108a2d117c31601fa86a1b8a
Melito <premiumm9@gmail.com>
2025-05-19 02:39:20 -0700

    metodo pago update

---
+++ src/components/metodo_pago/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btnConfig = document.getElementById("btnConfig");
+    const menuDisplay = document.getElementById("menuDisplay");
+    btnConfig.addEventListener("click", function(){
+        //Despliega el menu
+        menuDisplay.style.display = (menuDisplay.style.display === "none" || menuDisplay.style.display === "") ? "block" : "none";
+    });
+
+    document.addEventListener("click", function(event){
+        if (!btnConfig.contains(event.target) && !menuDisplay.contains(event.target)) {
+            menuDisplay.style.display = "none";
+        }
+    });
+});
