1a46dc8a010ae244f04b82a4e3be0a29f812b0e1
Melito <premiumm9@gmail.com>
2025-05-19 19:02:39 -0700

    update metodo Pago

--- src/components/metodo_pago/index.js
+++ src/components/metodo_pago/index.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const btnConfig = document.getElementById("btnConfig");
     const menuDisplay = document.getElementById("menuDisplay");
+    const btnBack = document.getElementById("back");
+    //Permite que el boton regrese al menu de cliente
+    btnBack.addEventListener("click", ()=> {
+        window.location.href="../inicio/inicioCliente/inicioCliente.html";
+    });
     btnConfig.addEventListener("click", function(){
         //Despliega el menu
         menuDisplay.style.display = (menuDisplay.style.display === "none" || menuDisplay.style.display === "") ? "block" : "none";
@@ -11,4 +16,12 @@ document.addEventListener("DOMContentLoaded", function () {
             menuDisplay.style.display = "none";
         }
     });
+    
 });
+    function formatoTarjeta(input) {
+        //Elimina caracteres y letras
+        let numTarjeta = input.value.replace(/\D/g, '');
+        //Separa cada 4 numeros
+        let formatoTar = numTarjeta.match(/.{1,4}/g)?.join('-');
+        input.value = formatoTar || '';
+    }
