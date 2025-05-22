fae7ec6303133c4b16a3882ef7e727a234fa7add
Melito <premiumm9@gmail.com>
2025-05-22 03:53:38 -0700

    correciones menu, botones y referencias

--- src/components/Chats/chat.js
+++ src/components/Chats/chat.js
@@ -1,6 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const btnBack = document.getElementById("back");
-    btnBack.addEventListener("click", ()=> {
-        window.location.href="../inicio/inicioVendedor/inicioVendedor.html";
+    const btnBackV = document.getElementById("backV");
+    const btnBackU = document.getElementById("backU");
+
+    if (btnBackU) {
+        btnBackU.addEventListener("click", ()=> {
+            if (window.history.length > 1) {
+                window.history.back();
+            } else {
+                window.location.href="../inicio/inicioCliente/inicioCliente.html";
+            }
+        });
+    } else if(btnBackV) {
+        btnBackV.addEventListener("click", ()=> {
+            if (window.history.length > 1) {
+                window.history.back();
+            } else {
+                window.location.href="../inicio/inicioVendedor/inicioVendedor.html";
+            }
     });
+    }
 });
